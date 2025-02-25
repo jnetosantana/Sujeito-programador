4505474e1783e50bd1c5e6b802a5c47ee8bf29bc
João Neto <144806481+jnetosantana@users.noreply.github.com>
2025-02-25 20:35:04 -0300

    conhecendo loop

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+/* var x = 0;
+
+while (x < 10){
+    document.write("<br> O valor do x é:" + x)
+
+    x++;
+}*/
+
+for(a = 0; a < valor; a++){
+    document.write("<br> Valor do A é:" + a)
+    
+}
