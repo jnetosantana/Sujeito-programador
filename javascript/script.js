f112155aa424ecede35239618503f78c0d405216
João Neto <144806481+jnetosantana@users.noreply.github.com>
2025-02-26 10:59:20 -0300

    switch

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,17 @@
-/* var x = 0;
+function pedir() {
+    var valor = prompt ("Digite um valor de 1 a 4")
 
-while (x < 10){
-    document.write("<br> O valor do x é:" + x)
-
-    x++;
-}*/
-
-for(a = 0; a < valor; a++){
-    document.write("<br> Valor do A é:" + a)
-    
+    switch(Number(valor)){
+        case 1:
+            alert("Você escolheu 1 = Suco")
+            break;
+        case 2:
+            alert("Você escolheu 2 = Água Gelada")
+            break;
+        case 3:
+            alert("Você escolheu 3 = Sorvete")
+            break;
+        case 4:
+            alert("Você escolheu 4 = Chamar o Garçom")
+    }
 }
