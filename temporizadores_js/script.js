6d36c9cb90fb77337753f3de7e297ea51ed863db
João Neto <144806481+jnetosantana@users.noreply.github.com>
2025-03-11 17:55:35 -0300

    timeout, set interval

---
+++ temporizadores_js/script.js
@@ -0,0 +1,11 @@
+function action() {
+    document.write("Executando... <br/>")
+}
+
+//setInterval(() => {
+//    document.write("Executando!!! <br/>")
+//}, 1000)
+
+setTimeout(() => {
+    console.log("EXECUTOU NOSSO TIMEOUT")
+}, 3000);
