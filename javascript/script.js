7b35e96e6b280b4cb6929f7d52cae3519b6b8729
João Neto <144806481+jnetosantana@users.noreply.github.com>
2025-03-11 09:28:57 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -28,5 +28,6 @@ function calcular (event) {
         resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado, Obesidade!'
     }
 
-
+    document.getElementById('peso').value = '';
+    document.getElementById('altura').value = '';
 }
