75c2a9e4420bc20b0b89f7457ad11b2d568983de
João Neto <144806481+jnetosantana@users.noreply.github.com>
2025-03-26 09:36:26 -0300

    .

---
+++ includes_endswith_startswith/script.js
@@ -0,0 +1,13 @@
+//let nomes = ["Matheus", "Lucas", "Jose"]
+
+///console.log(nomes.includes("Matheus"))
+
+//if(nomes.includes("Matheus")){
+//    console.log("ESTA NA LIST")
+//}else {
+//    console.log("NÃO ESTA NA LISTA")
+//}
+
+let nomes = "Matheus"
+
+console.log(nomes.startsWith("Mat"))
---
+++ operacoes_em_um_array/script.js
@@ -0,0 +1,7 @@
+let listagem = [5, 3, "Jose", 2, "Matheus"]
+
+let busca = listagem.find((item) => {
+    return item === "Jose"
+})
+
+console.log(busca)
